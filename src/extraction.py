4d2d182a3b08e727ce5d893ba849289811941f39
Thomas Nuttall <thomasnuttall8@gmail.com>
2019-03-19 16:36:07 +0100

    Flexible word grams

--- src/extraction.py
+++ src/extraction.py
@@ -1,4 +1,4 @@
-def extract_pattern_grams(notes, n=2):
+def extract_pattern_grams(notes, min_n=2, max_n=2):
     """
     For a list of list of notes, <notes>
     Extract all possible note-grams up to a maximum length of <n>
@@ -8,7 +8,7 @@ def extract_pattern_grams(notes, n=2):
     comb  = []
     for i in range(num_notes):
         # Final n patterns are counted more than once
-        n_ = num_notes - i if n > num_notes - i else n
+        n_ = num_notes - i if max_n > num_notes - i else max_n
         comb.append([notes[i:i+j] for j in range(2,n_+1)])
     flat = [i for c in comb for i in c]
-    return ' '.join([''.join(x) for x in flat if len(x) > 1 if 'R' not in x])
+    return ' '.join([''.join(x) for x in flat if len(x) >= min_n if 'R' not in x])
